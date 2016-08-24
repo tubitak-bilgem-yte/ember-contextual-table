afd1653bb36aa3dc349568b17e6d1b425e0f9550
ykaragol <ykaragol@gmail.com>
2016-08-24 17:25:26 +0300

    Use col.rowIndex instead of col.index

--- tests/integration/components/dt-column-cell-test.js
+++ tests/integration/components/dt-column-cell-test.js
@@ -17,8 +17,8 @@ test('renders in basic form', function(assert) {
 
 test('renders in block form', function(assert) {
   this.render(hbs`
-   {{#dt-column-cell row=row index=10 as |column|}}
-     {{column.row.surname}}-{{column.index}}-{{column.body}}
+   {{#dt-column-cell row=row rowIndex=10 as |column|}}
+     {{column.row.surname}}-{{column.rowIndex}}-{{column.body}}
    {{/dt-column-cell}}
    `);
   assert.equal(this.$('td').text().trim(), 'Sneijder-10-true');
