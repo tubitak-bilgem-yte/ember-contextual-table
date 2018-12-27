c2c2f37d83514e2cb6f4600016e01656a4a1ac94
ahmetemrekilinc <aekilinc@gmail.com>
2018-12-27 16:48:06 +0300

    3.0.3 eslint fixed.

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,22 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+  rules: {
+    'attribute-indentation': false,
+    'block-indentation': false,
+    'link-rel-noopener': false,
+    'no-bare-strings': false,
+    'no-html-comments': false,
+    'no-inline-styles': false,
+    'no-invalid-interactive': false,
+    'no-log': false,
+    'no-triple-curlies': false,
+    'no-unnecessary-concat': false,
+    'no-unused-block-params': false,
+    'quotes' : undefined,
+    'self-closing-void-elements': false,
+    'style-concatenation': false,
+    'table-groups': false
+  }
 };
