9942a5162c2af5f4ec9d4947ea8ae96e090b2b15
eztaskomaz <eztaskomaz@gmail.com>
2020-12-08 20:09:03 +0300

    4.0.3

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name,
-
-  isDevelopingAddon : function(){
-    return true;
-  },
-  hintingEnabled: function() {
-    return false;
-  }
+  name: require('./package').name
 };
